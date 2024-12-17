425565cba0fb2fbe0fb5d685c2236bf5f8913678
Sukrita <sugritabursuk@gmail.com>
2024-12-17 16:28:28 +0700

    lab10_3 completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,22 +1,33 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <ostream>
+
 using namespace std;
 
-int main (){
-	ifstream source;
-	ofstream dest;
-	source.open("cheerbook.txt");
-	dest.open("cheerbook_copy.txt");
-	
-    dest << "-------------------- BOOM ---------------------" << endl;
-    string textline;
-    while (getline(source, textline)) {
-        dest << textline << endl;
-    }
-    dest << "-------------------- HA!! ---------------------" << endl;
+int main()
 
-    source.close();
-    dest.close();
-	return 0;
+{
+        int count = 0;
+        float sum = 0;
+        float sum_of_square = 0;
+        string textline;
+        ifstream source("score.txt");
+        while (getline(source,textline))
+        {
+                float num = stof(textline);
+                sum += num;
+                sum_of_square += num*num;
+                count++;
+        }
+        float mean = sum / count;
+        float variance = (sum_of_square / count) - (mean * mean);
+        float standard_deviation = sqrt(variance);
+
+        cout << "Number of data = " << count << "\n";
+        cout << setprecision(3);
+        cout << "Mean = " << mean << "\n";
+        cout << "Standard deviation = " << standard_deviation;
 }
